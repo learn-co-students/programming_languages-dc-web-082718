1cdf580fcf099b078465701a5f7a4abd5ea68cb8
github username <github email address>
2018-08-22 19:19:24 -0400

    Developed method to reconstruct hash.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {};
+  temp_array = [];
+  first_pass = false
+  languages.each do |tier_one_key, tier_one_value|
+    tier_one_value.each do |language, attributes|
+      if new_hash[language] == nil
+        new_hash[language] = attributes
+      end
+      if new_hash[language][:style] == nil
+        temp_array.push(tier_one_key)
+        new_hash[language][:style] = temp_array
+      end
+      temp_array = new_hash[language][:style]
+      if !temp_array.include?(tier_one_key)
+        temp_array.push(tier_one_key)
+        new_hash[language][:style] = temp_array
+      end
+      temp_array = [];
+    end
+    first_pass = true
+  end
+
+  new_hash
 end
