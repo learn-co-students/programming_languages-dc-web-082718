a7aa601785a18d9a17f2a7537baa485a7c2bce43
angelica winkler <caw400@nyu.edu>
2018-08-22 17:51:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,20 +1,14 @@
-def reformat_languages(languages)
-  language_attributes = {}
+require "pry"
 
-  languages.each do |oo_or_functional, language_hash|
-    language_hash.each do |language, attribute_hash|
-      attribute_hash.each do |attribute, str_value|
-        if language_attributes[language].nil?
-          language_attributes[language] = {}
-        end
+def reformat_languages (languages)
+  new_hash = {}
 
-        language_attributes[language][:style] ||= []
-        language_attributes[language][:style] << oo_or_functional
-        if language_attributes[language][attribute].nil?
-          language_attributes[language][attribute] = str_value
-        end
-      end
+  languages.each do |style, language_hash|
+    language_hash.each do |lang, type_hash|
+      new_hash[lang] ||= type_hash
+      new_hash[lang][:style] ||= []
+      new_hash[lang][:style] << style
     end
   end
-  language_attributes
+  new_hash
 end
